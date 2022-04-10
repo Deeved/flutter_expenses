d5f5fa91df07611780f6a2ef22bb6ba32f5630ac
Deeved Hiuston <deeved@gmail.com>
2022-04-10 10:49:05 -0300

    Classe modelo Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(const ExpensesApp());
@@ -8,7 +9,7 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: MyHomePage(),
     );
   }
@@ -16,7 +17,21 @@ class ExpensesApp extends StatelessWidget {
 
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'Novo Tênis de Corrida', 
+      value: 310.76, 
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Conta de luz', 
+      value: 211.30, 
+      date: DateTime.now(),
+    )
+  ];
+  
 
   @override
   Widget build(BuildContext context) {
