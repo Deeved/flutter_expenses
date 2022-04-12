41b2e8c70519597d4f7406652c78e58e8bc2f9b5
Deeved Hiuston <deeved@gmail.com>
2022-04-12 20:55:07 -0300

    Item lista personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,72 @@
-
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({ Key? key }) : super(key: key);
+  const ExpensesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
-
 class MyHomePage extends StatelessWidget {
   final _transactions = [
     Transaction(
-      id: 't1', 
-      title: 'Novo Tênis de Corrida', 
-      value: 310.76, 
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
       date: DateTime.now(),
     ),
     Transaction(
-      id: 't2', 
-      title: 'Conta de luz', 
-      value: 211.30, 
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
       date: DateTime.now(),
     )
   ];
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Despesas Pessoais'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
             ),
-          ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Text(tr.title),
-              );
-            }).toList(),
-          )
-        ],
-      )
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(tr.value.toString()),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(tr.title),
+                          Text(tr.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
