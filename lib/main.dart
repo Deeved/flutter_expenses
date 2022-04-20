dbe6c57539d0257a9a0f22a64044137b35dc800e
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-04-20 16:52:38 -0300

    Botões para adicionar modal

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -36,6 +42,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
