0715bc3e416d62f699decb8ea57cd2ae676817fd
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-04-20 12:54:21 -0300

    Usando ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,55 +11,55 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 320,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      width: 96,
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 96,
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
                       ),
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tr.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: TextStyle(color: Colors.grey[400]),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: TextStyle(color: Colors.grey[400]),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
